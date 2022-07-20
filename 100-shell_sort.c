111f1cf1acf421bddfa3b467034db529dc6ae028
marceesty <marceesty@gmail.com>
2022-07-20 01:32:40 +0100

    100 updated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,28 +1,63 @@
 #include "sort.h"
 /**
-  * shell_sort - shell sort, knuth sequence
-  * @array: array to be sorted
-  * @size: size of array
-  */
-void shell_sort(int *array, size_t size)
+ * _swap - swap two numbers.
+ * @a: integer
+ * @b: integer
+ **/
+
+void _swap(int *a, int *b)
 {
-	size_t knuth, i, i2;
 	int tmp;
 
-	knuth = 1;
-	while (knuth < size)
-		knuth = (knuth * 3) + 1;
-	knuth = (knuth - 1) / 3;
-	while (knuth > 0)
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * backward_insertion -swap two nodes right left position
+ * @array: array
+ * @gap: gap
+ * @act: actual position in the array
+ **/
+void backward_insertion(int *array, int gap, int act)
+{
+	int i;
+
+	for (i = act - gap; i >= 0; i -= gap, act -= gap)
 	{
-		for (i = knuth; i < size; i++)
+		if (array[i] > array[act])
+			_swap(&array[i], &array[act]);
+		else
+			break;
+	}
+}
+/**
+ * shell_sort -Sort an array using shell_sort algorithm
+ * @array: array
+ * @size: size
+ **/
+void shell_sort(int *array, size_t size)
+{
+	unsigned int gap = 1, i, j;
+
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = 0, j = gap; j < size; i++, j++)
 		{
-			tmp = array[i];
-			for (i2 = i; i2 >= knuth && array[i2 - knuth] > tmp; i2 -= knuth)
-				array[i2] = array[i2 - knuth];
-			array[i2] = tmp;
+			if (array[i] > array[j])
+			{
+				_swap(&array[i], &array[j]);
+				backward_insertion(array, gap, i);
+			}
 		}
-		knuth = (knuth - 1) / 3;
 		print_array(array, size);
+		gap /= 3;
 	}
 }
